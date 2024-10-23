c71f17c094de8a17a2de5f930623ad8df6ed19cb
zezeze97 <18428308691@163.com>
2024-10-23 14:47:19 +0800

    update

--- geo_showo_t2i.sh
+++ geo_showo_t2i.sh
@@ -1,4 +1,4 @@
-python3 inference_t2i.py config=configs/geo_showo_demo_512x512.yaml \
+python3 inference_t2i.py config=configs/geo_showo_demo.yaml \
 batch_size=1 validation_prompts_file=validation_prompts/geoprompts.txt \
 guidance_scale=5 generation_timesteps=100 \
 mode='t2i'
